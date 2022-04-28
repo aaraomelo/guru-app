eefe3220163273799a6810ae04067a2efbe641db
Aarão Melo <aarao.melo@shiftinc.com.br>
2022-04-28 15:07:09 -0400

    Create custom input element

---
+++ components/CustomInput.tsx
@@ -0,0 +1,39 @@
+import { FormControl, FormErrorMessage, FormHelperText, FormLabel, Input } from "@chakra-ui/react"
+
+type Props = {
+  id: string;
+  type: string;
+  label: string;
+  value: any;
+  onChange?: (e: any) => void;
+  placeholder?: string;
+  helperText?: string;
+  errorMessages?: Array<string>;
+  mt?: number
+}
+
+const CustomInput = ({ id, type, label, value, onChange, placeholder, helperText, errorMessages, mt }: Props) => {
+  const isError = errorMessages?.length != 0;
+  return (
+    <FormControl mt={mt} isInvalid={isError}>
+      <FormLabel
+        htmlFor={id}
+        color={isError ? 'red.500' : 'black'}
+      >
+        {label}
+      </FormLabel>
+      <Input
+        id={id}
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        errorBorderColor='red.500'
+      />
+      {!isError ? (<FormHelperText>{helperText}</FormHelperText>) :
+        (<FormErrorMessage>{errorMessages && errorMessages[0]}</FormErrorMessage>)}
+    </FormControl>
+  )
+}
+
+export default CustomInput;
